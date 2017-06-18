b991c8bf0c1d35f9f7bbeaebe7eb8f744038df85
qxzhang <3165845957@qq.com>
2017-06-18 09:47:20 +0800

    输入输出

---
+++ IO.cpp
@@ -0,0 +1,12 @@
+#include<string>
+#include<cstdlib>
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<<"请输入你的名字"<<endl;
+	string name;
+	cin>>name;
+	cout<<"hello "<<name<<endl;
+	return 0;
+}
